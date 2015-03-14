9dfd1ebdf838bb0c371e19e0597ae5e0b18d4700
Edward Sum Lok Yu <syu72@gatech.edu>
2015-03-14 03:51:51 -0400

    Initial Commit

---
+++ WatchKitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchKitDemo
+//
+//  Created by Edward Yu on 14/3/15.
+//  Copyright (c) 2015 Edward Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
